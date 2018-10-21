522d31cce959143d3550bb6dc4de57a6daede670
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-10-21 19:42:28 +0700

    [UPDATE] master : del start and end at

--- microservice/RegisterService/src/main/java/me/wasin/RegisterService/Register/Register.java
+++ microservice/RegisterService/src/main/java/me/wasin/RegisterService/Register/Register.java
@@ -12,8 +12,6 @@ import java.util.Date;
 
 @Entity
 @Table(name = "registers")
-@EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Register implements Serializable {
 
     @Id
@@ -23,70 +21,44 @@ public class Register implements Serializable {
     @NotBlank
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "student_id", nullable = false)
-    private User student_id;
+    private User studentId;
 
     @NotBlank
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "elective_id", nullable = false)
-    private Elective elective_id;
-
-    @NotBlank
-    @Column(name = "start_at")
-    private Date startAt;
-
-    @NotBlank
-    @Column(name = "end_at")
-    private Date endAt;
+    private Elective electiveId;
 
     public Register() {
         super();
     }
 
-    public Register(@NotBlank User student_id, @NotBlank Elective elective_id, @NotBlank Date startAt, @NotBlank Date endAt) {
-        this.student_id = student_id;
-        this.elective_id = elective_id;
-        this.startAt = startAt;
-        this.endAt = endAt;
+    public Register(@NotBlank User studentId, @NotBlank Elective electiveId) {
+        this.studentId = studentId;
+        this.electiveId = electiveId;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public void setStudent_id(User student_id) {
-        this.student_id = student_id;
+    public void setStudentId(User studentId) {
+        this.studentId = studentId;
     }
 
-    public void setElective_id(Elective elective_id) {
-        this.elective_id = elective_id;
-    }
-
-    public void setStartAt(Date startAt) {
-        this.startAt = startAt;
-    }
-
-    public void setEndAt(Date endAt) {
-        this.endAt = endAt;
+    public void setElectiveId(Elective electiveId) {
+        this.electiveId = electiveId;
     }
 
     public int getId() {
         return id;
     }
 
-    public User getStudent_id() {
-        return student_id;
+    public User getStudentId() {
+        return studentId;
     }
 
-    public Elective getElective_id() {
-        return elective_id;
-    }
-
-    public Date getStartAt() {
-        return startAt;
-    }
-
-    public Date getEndAt() {
-        return endAt;
+    public Elective getElectiveId() {
+        return electiveId;
     }
 
 }
