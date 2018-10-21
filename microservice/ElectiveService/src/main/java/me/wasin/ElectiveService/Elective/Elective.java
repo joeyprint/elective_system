cb093b4edecf575ccc2794f0db74172cacb90aae
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-10-21 16:30:18 +0700

    [CREATE] master : create elective model

---
+++ microservice/ElectiveService/src/main/java/me/wasin/ElectiveService/Elective/Elective.java
@@ -0,0 +1,56 @@
+package me.wasin.ElectiveService.Elective;
+
+import me.wasin.ElectiveService.Subject.Subject;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Entity
+public class Elective implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subject_id", nullable = false)
+    private Subject subject;
+
+    @NotBlank
+    @Column(name = "total_register")
+    private int totalRegister;
+
+    public Elective() {
+        super();
+    }
+
+    public Elective(Subject subject, @NotBlank int totalRegister) {
+        this.subject = subject;
+        this.totalRegister = totalRegister;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public void setTotalRegister(int totalRegister) {
+        this.totalRegister = totalRegister;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public int getTotalRegister() {
+        return totalRegister;
+    }
+}
