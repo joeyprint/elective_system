e1d2cb02135e7e86f2718e7279b46649409919a0
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-10-21 15:41:09 +0700

    [INIT] master : init elective service

---
+++ microservice/ElectiveService/src/main/java/me/wasin/ElectiveService/ElectiveServiceApplication.java
@@ -0,0 +1,12 @@
+package me.wasin.ElectiveService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectiveServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectiveServiceApplication.class, args);
+	}
+}
