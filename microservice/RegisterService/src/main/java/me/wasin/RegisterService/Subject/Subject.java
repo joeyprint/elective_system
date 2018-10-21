79b6ddb5dfaabd490d067672183bf38d7b57067d
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-10-21 18:04:27 +0700

    [CREATE] master : add subject model into register service

---
+++ microservice/RegisterService/src/main/java/me/wasin/RegisterService/Subject/Subject.java
@@ -0,0 +1,71 @@
+package me.wasin.RegisterService.Subject;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Subject implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    @Column(name = "subject_id")
+    private String subjectId;
+
+    @NotBlank
+    @Column(name = "subject_name")
+    private String subjectName;
+
+    @Column(name = "lecturer_id")
+    private int lecturerId;
+
+    public Subject() {
+        super();
+    }
+
+    public Subject(@NotBlank String subjectId, @NotBlank String subjectName, @NotBlank int lecturerId) {
+        this.subjectId = subjectId;
+        this.subjectName = subjectName;
+        this.lecturerId = lecturerId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSubjectId(String subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public void setLecturerId(int lecturerId) {
+        this.lecturerId = lecturerId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public int getLecturerId() {
+        return lecturerId;
+    }
+}
