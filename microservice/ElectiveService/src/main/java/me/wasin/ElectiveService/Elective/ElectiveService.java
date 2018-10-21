a65acf571841d5e7f85471bb8b60c4cfa06ac048
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-10-21 16:31:26 +0700

    [CREATE] master : create elective service

---
+++ microservice/ElectiveService/src/main/java/me/wasin/ElectiveService/Elective/ElectiveService.java
@@ -0,0 +1,38 @@
+package me.wasin.ElectiveService.Elective;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ElectiveService {
+
+    @Autowired
+    private ElectiveRepository electiveRepository;
+
+    public List<Elective> getAllElective() {
+        return electiveRepository.findAll();
+    }
+
+    public Elective getElectiveById(int id) {
+        return electiveRepository.findById(id);
+    }
+
+    public Elective createElective(Elective elective) {
+        return electiveRepository.save(elective);
+    }
+
+    public Elective updateTotalRegister(int id, Elective electiveRequest) {
+        Elective elective = electiveRepository.findById(id);
+        elective.setTotalRegister(electiveRequest.getTotalRegister());
+        return electiveRepository.save(elective);
+    }
+
+    public Elective deleteElective(int id) {
+        Elective elective = electiveRepository.findById(id);
+        electiveRepository.delete(elective);
+        return elective;
+    }
+
+}
